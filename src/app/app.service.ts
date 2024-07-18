a18bb87638b1cd0a8227cf5a24af92135724c6c7
Luqman Hakim <luqeckr@gmail.com>
2024-07-18 15:20:07 +0800

    total price now in service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,8 +6,7 @@ import { CartItem, Product } from './product.model';
 export class AppService {
   cartCount$ = new BehaviorSubject(0)
   cart: CartItem[] = []
-  cartList: Product[] = []
-  // cartList$ = new BehaviorSubject<Cart[] | null>(this.cartList)
+  totalPrice$ = new BehaviorSubject(0)
 
   constructor() { }
 
@@ -18,6 +17,11 @@ export class AppService {
   getCartCount() {
     return this.cartCount$.asObservable();
   }
+  
+  getTotalPrice() {
+    return this.totalPrice$.asObservable();
+  }
+
 
   addToCart(product: Product | undefined) {
     if (product) {
@@ -34,17 +38,21 @@ export class AppService {
   removeFromCart(product: Product) {
     const cartItem = this.cart.find(item => item.product.id === product.id);
     if (cartItem) {
-      cartItem.quantity--;
-      if (cartItem.quantity === 0) {
-        this.cart = this.cart.filter(item => item.product.id !== product.id);
-      }
+      this.cart = this.cart.filter(item => item.product.id !== product.id);
       this.updateCartCount();
     }
+  }
+
+  updateCart(updatedCart: CartItem[]) {
+    this.cart = updatedCart;
+    this.updateCartCount();
   }
 
   private updateCartCount() {
     const totalQuantity = this.cart.reduce((acc, item) => acc + item.quantity, 0);
     this.cartCount$.next(totalQuantity);
+    const totalPrice = this.cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+    this.totalPrice$.next(Math.round(totalPrice*100)/100)
   }
   
 }
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,42 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 import { CartItem, Product } from '../product.model';
+import { NgStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: true,
-  imports: [],
+  imports: [NgStyle, FormsModule],
   templateUrl: './shopping-cart.component.html',
   // styleUrl: './shopping-cart.component.scss'
 })
 export class ShoppingCartComponent implements OnInit {
 
   cart: CartItem[] = []
-  totalPrice: number = 0;
+  dollarKeRupiah = 16000
+  totalPrice: string = '0'
   
   constructor(
     private _svc: AppService
-  ) {}
+  ) {
+    this._svc.totalPrice$.subscribe(r => this.totalPrice = (r * this.dollarKeRupiah).toLocaleString('id-ID'))
+  }
 
   ngOnInit(): void {
     this.cart = this._svc.getCart()
-    this.calculateTotalPrice();
   }
 
-  addToCart(product: Product) {
-    this._svc.addToCart(product);
-    this.cart = this._svc.getCart();
-    this.calculateTotalPrice();
+  onQuantityChange(product: Product, quantity: number) {
+    const cartItem = this.cart.find(item => item.product.id === product.id);
+    if (cartItem) {
+      cartItem.quantity = quantity;
+      this._svc.updateCart(this.cart);
+    }
   }
 
   removeFromCart(product: Product) {
-    this._svc.removeFromCart(product)
-    this.cart = this._svc.getCart()
-    this.calculateTotalPrice();
-  }
-
-  calculateTotalPrice() {
-    this.totalPrice = this.cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+    this._svc.removeFromCart(product);
+    this.cart = this._svc.getCart();
   }
 
 }
