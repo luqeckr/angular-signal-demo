e3dc6a77d8b3e075baa16d1685e0bb07037543df
Luqman Hakim <luqeckr@gmail.com>
2024-07-18 15:19:06 +0800

    get totalprice from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppService } from './app.service';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,11 @@ import { AsyncPipe } from '@angular/common';
 export class AppComponent {
   title = 'signal-sample';
   cartCount = this._svc.cartCount$
+  totalPrice: Observable<number>
   
   constructor(
     private _svc: AppService
   ) {
+    this.totalPrice = this._svc.totalPrice$
   }
 }
