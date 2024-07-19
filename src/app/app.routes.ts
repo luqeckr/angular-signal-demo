8886ae3d666fc62271285dde8057e2741c0e96a7
Luqman Hakim <luqeckr@gmail.com>
2024-07-19 15:07:19 +0800

    fix flickering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent, children:[
+    {path: ':id', component: ProductsComponent}
+  ] },
   { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
